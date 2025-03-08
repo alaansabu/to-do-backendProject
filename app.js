8b2d68b2ac65fae96ec7a9fb7238a2989e95feb9
alaansabu <alansabu6046@gmail.com>
2025-03-09 01:13:46 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+
+app.get("/",(req,res) =>{
+
+    res.send("Hello word")
+})
+
+app.listen(PORT,()=>{
+
+console.log(`Server running in port ${PORT}🚀`);
+
+
+})
